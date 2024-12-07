1d5ac62d278fb6818bf4239b9a37771c283e4250
chathuwa-whiz <it22049872@my.sliit.lk>
2024-12-07 22:43:46 +0530

    ppdf done

--- views/modules/products.php
+++ views/modules/products.php
@@ -24,7 +24,7 @@ if($_SESSION["profile"] == "Seller"){
     </h1>
 
     <ol class="breadcrumb">
-		
+        
       <li><a href="home"><i class="fa fa-dashboard"></i> Home</a></li>
 
       <li class="active">Dashboard</li>
@@ -40,15 +40,16 @@ if($_SESSION["profile"] == "Seller"){
       <div class="box-header with-border">
 
         <button class="btn btn-success" data-toggle="modal" data-target="#addProduct"> <i class="fa fa-plus"></i> Add Product</button>
+        <button onclick="exportTableToExcel('productsTable', 'products_report')" class="btn btn-primary" style="margin-left: 10px;">Download Report</button>
 
       </div>
 
       <div class="box-body">
 
-        <table class="table table-bordered table-hover table-striped dt-responsive productsTable" width="100%">
+        <table class="table table-bordered table-hover table-striped dt-responsive productsTable" id="productsTable" width="100%">
        
           <thead>
-			
+            
            <tr>
              
              <th style="width:10px">#</th>
@@ -62,22 +63,91 @@ if($_SESSION["profile"] == "Seller"){
              <th>Discount</th>
              <th>Date added</th>
              <th>Actions</th>
-
            </tr> 
-
           </thead>
+          <tbody>
+            <?php
+            // Fetch the product data
+            $products = controllerProducts::ctrShowProducts($item, $value, $order);
 
+            // Display the product data
+            foreach ($products as $product) {
+                echo '
+                <tr>
+                    <td>' . $product["id"] . '</td>
+                    <td><img src="' . $product["image"] . '" class="img-thumbnail" width="40px"></td>
+                    <td>' . $product["code"] . '</td>
+                    <td>' . $product["description"] . '</td>
+                    <td>' . $product["category"] . '</td>
+                    <td>' . $product["stock"] . '</td>
+                    <td>' . $product["buyingPrice"] . '</td>
+                    <td>' . $product["sellingPrice"] . '</td>
+                    <td>' . $product["discount"] . '</td>
+                    <td>' . $product["date_added"] . '</td>
+                    <td>
+                      <div class="btn-group">
+                        <button class="btn btn-primary btnEditProduct" idProduct="' . $product["id"] . '" data-toggle="modal" data-target="#modalEditProduct"><i class="fa fa-pencil"></i></button>
+                        <button class="btn btn-danger btnDeleteProduct" idProduct="' . $product["id"] . '" code="' . $product["code"] . '" image="' . $product["image"] . '"><i class="fa fa-trash"></i></button>
+                      </div>
+                    </td>
+                </tr>';
+            }
+            ?>
+          </tbody>
         </table>
-
-        <input type="hidden" value="<?php echo $_SESSION['profile']; ?>" id="hiddenProfile">
-
       </div>
     
     </div>
-
   </section>
-
 </div>
+
+<!-- Export to Excel Function -->
+<script>
+function exportTableToExcel(tableID, filename = ''){
+    var downloadLink;
+    var dataType = 'application/vnd.ms-excel';
+    var tableSelect = document.getElementById(tableID);
+    var tableHTML = '<table border="1"><thead><tr><th>Code</th><th>Description</th><th>Stock</th></tr></thead><tbody>';
+
+    // Loop through the table rows and extract the required columns
+    var rows = tableSelect.getElementsByTagName('tr');
+    for (var i = 1; i < rows.length; i++) { // Start from 1 to skip the header row
+        var cells = rows[i].getElementsByTagName('td');
+        if (cells.length > 0) {
+            tableHTML += '<tr>';
+            tableHTML += '<td>' + cells[2].innerText + '</td>'; // Code
+            tableHTML += '<td>' + cells[3].innerText + '</td>'; // Description
+            tableHTML += '<td>' + cells[5].innerText + '</td>'; // Stock
+            tableHTML += '</tr>';
+        }
+    }
+    tableHTML += '</tbody></table>';
+
+    // Specify file name
+    filename = filename ? filename + '.xls' : 'products_report_' + Date.now() + '.xls';
+    
+    // Create download link element
+    downloadLink = document.createElement("a");
+    
+    document.body.appendChild(downloadLink);
+    
+    if(navigator.msSaveOrOpenBlob){
+        var blob = new Blob(['\ufeff', tableHTML], {
+            type: dataType
+        });
+        navigator.msSaveOrOpenBlob(blob, filename);
+    } else {
+        // Create a link to the file
+        downloadLink.href = 'data:' + dataType + ', ' + tableHTML;
+    
+        // Setting the file name
+        downloadLink.download = filename;
+        
+        // Triggering the function
+        downloadLink.click();
+    }
+}
+</script>
 
 <!--=====================================
 =            module add Product            =
